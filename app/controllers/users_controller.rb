3fdbcfb3407c70fb893398d25817bc335a18d739
plabradorjr <plabradorjr@gmail.com>
2020-02-07 08:48:38 -0600

    login done, CRUD next

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,4 +20,22 @@ class UsersController < ApplicationController
   end
 
 
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect to '/home'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/home"
+    else
+      redirect to '/signup'
+    end
+  end
+
 end 
