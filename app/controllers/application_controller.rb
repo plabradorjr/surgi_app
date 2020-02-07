280af8f6624dcfe5612157d9f81a814ffe6c948a
plabradorjr <plabradorjr@gmail.com>
2020-02-07 02:03:30 -0600

    added stylesheet

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < Sinatra::Base
+
+configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "surgiapp_secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+
+end 
