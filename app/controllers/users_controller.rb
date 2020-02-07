e9a5d05712dbb832600c6c510d3bf11f1f04b3a6
plabradorjr <plabradorjr@gmail.com>
2020-02-07 09:47:34 -0600

    /:slug added for listing user contents

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,6 +52,12 @@ class UsersController < ApplicationController
       redirect to '/'
     end
   end
-  
+
+  get '/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
+
+
 
 end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
     end
 
     def self.find_by_slug(slug)
-        User.all.find{|user| user.slug == slug}
+        User.all.find{|user| user.slug == slug.downcase}
     end
 
 end
