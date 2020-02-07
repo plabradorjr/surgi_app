11430bf45c6302387e31749eb848a6fc61512b47
plabradorjr <plabradorjr@gmail.com>
2020-02-07 12:06:17 -0600

    CRUD- update done, delete next

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -17,7 +17,7 @@ class NotesController < ApplicationController
     end
   end
 
-  post '/pages' do
+  post '/notes' do
     if logged_in?
       if params[:content] == "" || params[:topics] == ""
         redirect to "/note/new"
@@ -41,6 +41,46 @@ class NotesController < ApplicationController
     else
       redirect to '/login'
     end
+  end
+
+
+  get '/note/:id/edit' do
+    if logged_in?
+      @note = Note.find_by_id(params[:id])
+      if @note && @note.user == current_user
+        erb :'notes/edit_note'
+      else
+        redirect to '/home' #temporary redirect
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+
+  patch '/note/:id' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to "/note/#{params[:id]}/edit"
+      else
+        @note = Note.find_by_id(params[:id])
+        if @note && @note.user == current_user
+          if @note.update(content: params[:content], topics: params[:topics])
+            redirect to "/note/#{@note.id}"
+          else
+            redirect to "/note/#{@note.id}/edit"
+          end
+        else
+          redirect to '/prohibited' #temp redirect
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/prohibited' do
+      "You cant edit this, only creators of this content can edit this" #temporary, will edit later
   end
 
 
